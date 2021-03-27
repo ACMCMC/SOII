1194130ce5d80edb6013069ca52398f03ac29c52
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-03-27 15:11:37 +0100

    1

--- P3/1.c
+++ P3/1.c
@@ -4,6 +4,9 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
 
 #define MAX_SLEEP_PRODUCTOR 2
 #define MAX_SLEEP_CONSUMIDOR 3
@@ -23,7 +26,17 @@
 
 int *buffer, *cuenta;
 
-pid_t pid_productor, pid_consumidor;
+typedef struct
+{
+    enum
+    {
+        CONSUMIDOR,
+        PRODUCTOR
+    } tipo;
+    pid_t pid;
+} info_proceso;
+
+info_proceso *vector_info_procesos;
 
 void imprimir_buffer()
 {
@@ -134,38 +147,88 @@ void consumidor()
     printf("(C) He acabado!\n");
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    pid_t pid;
+    int fd_cuenta, fd_buffer, fd_pids;
+    struct stat fd_pifs_info, fd_cuenta_info;
+
+    if (argc != 2 || strncmp("-c", argv[1], 2) != 0 || strncmp("-p", argv[1], 2) != 0)
+    {
+        perror("Formato: %s [-c, para consumidor; -p, para productor]");
+        exit(EXIT_SUCCESS);
+    }
 
     srand(clock()); // Semilla del generador aleatorio de números
 
-    // Mapeamos la memoria compartida entre los procesos
-    cuenta = (int *)mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
-    buffer = (int *)mmap(NULL, sizeof(int) * TAM_BUFFER, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    fd_cuenta = shm_open("cuenta", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    fd_buffer = shm_open("buffer", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    fd_pids = shm_open("pids", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
 
-    *cuenta = 0;
-
-    pid_productor = getpid();
-
-    pid = fork();
-
-    if (pid < 0)
+    if (!fd_buffer || !fd_cuenta || !fd_pids)
     {
-        perror("Error en fork()");
+        perror("Error en shm_open()");
         exit(EXIT_FAILURE);
     }
-    else if (pid == 0)
-    { // Hijo. Este va a ser el consumidor.
-        pid_consumidor = getpid();
+
+    if (fstat(fd_pids, &fd_pifs_info))
+    {
+        perror("Error en fstat()");
+        exit(EXIT_FAILURE);
+    }
+    if (fstat(fd_cuenta, &fd_cuenta_info))
+    {
+        perror("Error en fstat()");
+        exit(EXIT_FAILURE);
+    }
+
+    if (fd_cuenta_info.st_size != sizeof(int))
+    { // Si el fichero originalmente no tenía el tamaño para albergar un int, le ponemos nosotros ese tamaño. El valor del tamaño que tenía al principio lo guardamos para después.
+        if (ftruncate(fd_cuenta, sizeof(int)) == -1)
+        {
+            perror("Error en ftruncate()");
+            exit(EXIT_FAILURE);
+        }
+    }
+    if (ftruncate(fd_buffer, sizeof(int) * TAM_BUFFER) == -1)
+    {
+        perror("Error en ftruncate()");
+        exit(EXIT_FAILURE);
+    }
+    if (ftruncate(fd_pids, fd_pifs_info.st_size + sizeof(info_proceso)) == -1) // Añadimos sitio para un struct de información más
+    {
+        perror("Error en ftruncate()");
+        exit(EXIT_FAILURE);
+    }
+
+    // Mapeamos la memoria compartida entre los procesos
+    cuenta = (int *)mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, fd_cuenta, 0);
+    buffer = (int *)mmap(NULL, sizeof(int) * TAM_BUFFER, PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, fd_buffer, 0);
+    vector_info_procesos = (info_proceso *)mmap(NULL, sizeof(int) * TAM_BUFFER, PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, fd_pids, 0);
+
+    if (cuenta == MAP_FAILED || buffer == MAP_FAILED || vector_info_procesos == MAP_FAILED)
+    {
+        perror("Error en mmap()");
+        exit(EXIT_FAILURE);
+    }
+
+    if (fd_cuenta_info.st_size != sizeof(int))
+    { // Si el fichero originalmente no tenía el tamaño para albergar un int, entonces lo habremos expandido para eso, y ahora que lo hemos mapeado en memoria, escribiremos un 0 en él.
+        *cuenta = 0;
+    }
+
+    if (argv[1][1] == 'c')
+    {
         consumidor();
     }
     else
-    { // Padre. Va a ser el productor.
-        pid_consumidor = pid;
+    {
         productor();
     }
 
     munmap(buffer, sizeof(int) * TAM_BUFFER);
     munmap(cuenta, sizeof(int));
+
+    shm_unlink();
+
+    exit(EXIT_SUCCESS);
 }
