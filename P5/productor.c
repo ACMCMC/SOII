fde204d210d07e7454717763276da82715d9ff73
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-05-14 19:25:46 +0200

    fix

--- P5/productor.c
+++ P5/productor.c
@@ -27,12 +27,13 @@ int producir()
 void productor()
 {
     int num_elementos_restantes;     // El numero de elementos que faltan por producir
+    char mensaje_vacio_consumidor; // El mensaje con el elemento producido, que le mandamos al consumidor
     char mensaje_elemento_producido; // El mensaje con el elemento producido, que le mandamos al consumidor
     int item;                        // El ítem que le pasamos al consumidor
 
     for (num_elementos_restantes = DATOS_A_PRODUCIR; num_elementos_restantes > 0; num_elementos_restantes--) // Vamos a iterar mientras aún queden ítems por producir
     {
-        if (mq_receive(almacen1, NULL, 0, NULL))
+        if (mq_receive(almacen1, &mensaje_vacio_consumidor, sizeof(mensaje_vacio_consumidor), NULL))
         { // Recibimos un mensaje del consumidor. Si no hay un mensaje disponible, la función se bloquea hasta que se recibe un mensaje. Lo recibimos en la cola almacen1, y como no nos interesa guardarlo (es un mensaje vacío), el puntero al lugar donde guardarlo es NULL, la longitud del buffer al que apunta (NULL) es 0, y tampoco nos interesa saber la prioridad del mensaje (cuarto argumento, que es NULL también). Se supone que la prioridad siempre es la misma (0).
             perror("Error en mq_receive");
         }
@@ -51,7 +52,9 @@ void productor()
 
 int main(int argc, char **argv)
 {
-    struct mq_attr attr; // Estructura que almacena parámetros sobre la cola de mensajes, se usa a la hora de crearla
+    struct mq_attr attr; // Estructura que almacena parámetros sobre la cola de mensajes
+    attr.mq_maxmsg = MAX_BUFFER; // La cola tendrá un máximo de MAX_BUFFER mensajes.
+    attr.mq_msgsize = sizeof(char); // El tamaño de mensajes para ambas colas será de un char como máximo. Realmente, el consumidor envía mensajes vacíos al productor, pero simplemente ignoraremos su contenido.
 
     srand(clock()); // Semilla del generador aleatorio de numeros
 
@@ -59,13 +62,8 @@ int main(int argc, char **argv)
     mq_unlink("/ALMACEN1");
     mq_unlink("/ALMACEN2");
 
-    attr.mq_maxmsg = MAX_BUFFER; // El máximo de los buffers es MAX_BUFFER
-    attr.mq_msgsize = 0; // El tamaño de los mensajes para el productor será 0 (son mensajes vacíos)
-
     /* Apertura de los buffers */
     almacen1 = mq_open("/ALMACEN1", O_CREAT | O_RDONLY, 0777, &attr); // Creamos la cola de mensajes almacen1, nuestro acceso a ella será en modo sólo lectura
-
-    attr.mq_msgsize = sizeof(char); // El tamaño de mensajes para el consumidor será de un char
     almacen2 = mq_open("/ALMACEN2", O_CREAT | O_WRONLY, 0777, &attr); // Creamos la cola de mensajes almacen2, nuestro acceso a ella será en modo sólo escritura
     if ((almacen1 == -1) || (almacen2 == -1))
     {
