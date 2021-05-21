daa5bf091a5137e169d2ef77688a8b5546d85d1e
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-05-21 21:58:03 +0200

    8

--- P6/8.c
+++ P6/8.c
@@ -54,6 +54,7 @@ int escoger_sentido() {
 void cruzar_izquierda(int id) {
     sem_wait(mutex_cuenta_izquierda);
     *cuenta_izquierda++;
+    printf("cuenta i: %d\n", *cuenta_izquierda);
     if (*cuenta_izquierda == 1) {
         sem_wait(puente);
         printf(ANSI_COLOR_GREEN "[%d] Estan cruzando coches desde la IZQUIERDA\n" ANSI_COLOR_RESET, id);
@@ -72,6 +73,7 @@ void cruzar_izquierda(int id) {
 void cruzar_derecha(int id) {
     sem_wait(mutex_cuenta_derecha);
     *cuenta_derecha++;
+    printf("cuenta d: %d\n", *cuenta_derecha);
     if (*cuenta_derecha == 1) {
         sem_wait(puente);
         printf(ANSI_COLOR_GREEN "[%d] Estan cruzando coches desde la DERECHA\n" ANSI_COLOR_RESET, id);
