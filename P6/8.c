78d6b0ee5ab0386219193d2a79afc9d9d892de18
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-05-21 21:38:08 +0200

    8

--- P6/8.c
+++ P6/8.c
@@ -13,6 +13,9 @@
 #define TIEMPO_PASEAR 30
 #define NUM_COCHES 8
 
+#define IZQUIERDA 0
+#define DERECHA 1
+
 // Codigos de color para formatear la salida en consola
 #define ANSI_COLOR_RED "\x1b[31m"
 #define ANSI_COLOR_GREEN "\x1b[32m"
@@ -22,72 +25,76 @@
 #define ANSI_COLOR_CYAN "\x1b[36m"
 #define ANSI_COLOR_RESET "\x1b[0m"
 
-int *buffer; // El buffer compartido de memoria
-int *cuenta; // La variable cuenta
+int *cuenta_izquierda; // La variable cuenta
+int *cuenta_derecha; // La variable cuenta
 
-sem_t *empty, *mutex, *full, *num_procesos; // Variables semaforo
-
-// Imprime por pantalla una representacion grafica del buffer. La parte ocupada de la lista se imprime en rojo, la parte libre en azul.
-void imprimir_buffer()
-{
-    int i;
-    printf("\t-");
-    for (i = 0; i < TAM_BUFFER; i++)
-    {
-        printf("------");
-    }
-    printf("\n\t|");
-    for (i = 0; i < TAM_BUFFER; i++)
-    {
-        printf(ANSI_COLOR_BLUE " %-3d " ANSI_COLOR_RESET "|", buffer[i]);
-    }
-    printf("\n\t-");
-    for (i = 0; i < TAM_BUFFER; i++)
-    {
-        printf("------");
-    }
-    printf("\n\n");
-}
+sem_t *mutex_cuenta_izquierda, *mutex_cuenta_derecha, *puente, *cruzando, *num_procesos; // Variables semaforo
 
 // Espera entre 1 y 30 segundos
 void pasear() {
     sleep(((int)rand()) % (TIEMPO_PASEAR - 1) + 1); // Introducimos una espera aleatoria
 }
 
-// Espera 2 segundos
+// Funcion que hace que un coche cruce el puente
 void cruzar_puente() {
+    sem_wait(cruzando);
     sleep(TIEMPO_CRUZAR_PUENTE);
+    sem_post(cruzando);
 }
 
+// Devuelve IZQUIERDA o DERECHA aleatoriamente
+int escoger_sentido() {
+    return ((int)rand() % 2);
+}
+
+void cruzar_izquierda(int id) {
+    sem_wait(mutex_cuenta_izquierda);
+    *cuenta_izquierda++;
+    if (*cuenta_izquierda == 1) {
+        sem_wait(puente);
+    }
+    sem_post(mutex_cuenta_izquierda);
+    cruzar_puente();
+    sem_wait(mutex_cuenta_izquierda);
+    *cuenta_izquierda--;
+    if (*cuenta_izquierda == 0) {
+        sem_post(puente);
+    }
+    sem_post(mutex_cuenta_izquierda);
+}
+
+void cruzar_derecha(int id) {
+    sem_wait(mutex_cuenta_derecha);
+    *cuenta_derecha++;
+    if (*cuenta_derecha == 1) {
+        sem_wait(puente);
+    }
+    sem_post(mutex_cuenta_derecha);
+    cruzar_puente();
+    sem_wait(mutex_cuenta_derecha);
+    *cuenta_derecha--;
+    if (*cuenta_derecha == 0) {
+        sem_post(puente);
+    }
+    sem_post(mutex_cuenta_derecha);
+}
 
 // Implementa la funcion de las diapositivas
 void coche(int id)
 {
-        pasear(); // Empezamos paseando de 1 a 30 s
-        if (sem_wait(empty))
-        {
-            perror("Error en sem_wait");
-        }
-        if (sem_wait(mutex))
-        {
-            perror("Error en sem_wait");
-        }
-        printf("(C) Adquiero el mutex\n");
-        insertar_item_buffer(item);
-        (*cuenta)++;
-        printf("(P) Inserto: %d\n", item);
-        imprimir_buffer();
-        printf("(C) Libero el mutex\n");
-        if (sem_post(mutex))
-        {
-            perror("Error en sem_post");
-        }
-        if (sem_post(full))
-        {
-            perror("Error en sem_post");
-        }
+    int sentido;
+
+    pasear(); // Empezamos paseando de 1 a 30 s
+    sentido = escoger_sentido();
+    if (sentido == IZQUIERDA) {
+        printf("[%d] Voy a cruzar el puente desde la IZQUIERDA\n", id);
+        cruzar_izquierda(id);
+        printf("[%d] He cruzado el puente desde la IZQUIERDA\n", id);
+    } else {
+        printf("[%d] Voy a cruzar el puente desde la DERECHA\n", id);
+        cruzar_derecha(id);
+        printf("[%d] He cruzado el puente desde la DERECHA\n", id);
     }
-    printf("(P) He acabado!\n");
 }
 
 int main(int argc, char **argv)
@@ -110,17 +117,22 @@ int main(int argc, char **argv)
     // Opcion de reiniciar
     if (argc == 2 && (strncmp("-r", argv[1], 2) == 0))
     {
-        if (sem_unlink("/empty"))
+        if (sem_unlink("/mutex_cuenta_izquierda"))
         {
             perror("Error en sem_unlink()");
             exit(EXIT_FAILURE);
         }
-        if (sem_unlink("/full"))
+        if (sem_unlink("/mutex_cuenta_derecha"))
         {
             perror("Error en sem_unlink()");
             exit(EXIT_FAILURE);
         }
-        if (sem_unlink("/mutex"))
+        if (sem_unlink("/puente"))
+        {
+            perror("Error en sem_unlink()");
+            exit(EXIT_FAILURE);
+        }
+        if (sem_unlink("/cruzando"))
         {
             perror("Error en sem_unlink()");
             exit(EXIT_FAILURE);
@@ -133,35 +145,37 @@ int main(int argc, char **argv)
     }
 
     // Generamos los semaforos. O_CREAT no tiene efecto si el semaforo ya estaba creado.
-    mutex = sem_open("/mutex", 0);
+    puente = sem_open("/puente", 0);
     // Si la funcion falla, es porque los semaforos no estan creados, asi que los creamos
-    if (mutex == SEM_FAILED)
+    if (puente == SEM_FAILED)
     {
         printf("Semaforos creados.\n");
-        mutex = sem_open("/mutex", O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, 1); // Usamos O_CREAT | O_EXCL porque podria haber dos procesos que detectaran que los semaforos no existen al mismo tiempo. De esta forma, solo uno de ellos los creara y el otro saldra con codigo de error.
+        puente = sem_open("/mutex", O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, 1); // Usamos O_CREAT | O_EXCL porque podria haber dos procesos que detectaran que los semaforos no existen al mismo tiempo. De esta forma, solo uno de ellos los creara y el otro saldra con codigo de error.
         num_procesos = sem_open("/num_procesos", O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, 0);
-        empty = sem_open("/empty", O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, TAM_BUFFER);
-        full = sem_open("/full", O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, 0);
+        mutex_cuenta_izquierda = sem_open("/mutex_cuenta_izquierda", O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, 1);
+        mutex_cuenta_derecha = sem_open("/mutex_cuenta_derecha", O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, 1);
+        cruzando = sem_open("/cruzando", O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, 1);
     }
     else
     {
         // Abrimos el resto de semaforos normalmente, ya que estaban creados de antes
         num_procesos = sem_open("/num_procesos", 0);
-        empty = sem_open("/empty", 0);
-        full = sem_open("/full", 0);
+        mutex_cuenta_izquierda = sem_open("/mutex_cuenta_izquierda", 0);
+        mutex_cuenta_derecha = sem_open("/mutex_cuenta_derecha", 0);
+        cruzando = sem_open("/cruzando", 0);
     }
 
-    if (empty == SEM_FAILED || full == SEM_FAILED || mutex == SEM_FAILED || num_procesos == SEM_FAILED)
+    if (mutex_cuenta_derecha == SEM_FAILED || mutex_cuenta_izquierda == SEM_FAILED || cruzando == SEM_FAILED || puente == SEM_FAILED || num_procesos == SEM_FAILED)
     {
         perror("Error en sem_open()");
         exit(EXIT_FAILURE);
     }
 
     // Mapeamos la memoria compartida para los procesos. Ahora no hace falta un objeto anonimo compartido en memoria, ya que los hijos heredaran este mapeo que hacemos localmente.
-    buffer = (int *)mmap(NULL, sizeof(int) * TAM_BUFFER, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
-    cuenta = (int *)mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    cuenta_izquierda = (int *)mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    cuenta_derecha = (int *)mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
 
-    if (buffer == MAP_FAILED || cuenta == MAP_FAILED)
+    if (cuenta_derecha == MAP_FAILED || cuenta_izquierda == MAP_FAILED)
     {
         perror("Error en mmap()");
         exit(EXIT_FAILURE);
@@ -172,9 +186,10 @@ int main(int argc, char **argv)
         perror("Error en sem_post");
     }
 
-    *cuenta = 0; // Inicializamos la variable cuenta a 0. Deberia venir ya inicializada, pero creo que es buena practica asegurarnos.
+    *cuenta_izquierda = 0; // Inicializamos la variable cuenta a 0. Deberia venir ya inicializada, pero creo que es buena practica asegurarnos.
+    *cuenta_derecha = 0; // Inicializamos la variable cuenta a 0. Deberia venir ya inicializada, pero creo que es buena practica asegurarnos.
 
-    for ((i = 0, pid = -1); i < NUM_PRODUCTORES && pid != 0; i++)
+    for ((i = 0, pid = -1); i < NUM_COCHES && pid != 0; i++)
     {
         pid = fork();
         if (pid < 0)
@@ -187,31 +202,11 @@ int main(int argc, char **argv)
             {
                 perror("Error en sem_post");
             }
-            productor(i==0 ? ((NUM_ELEMENTOS_TOTALES / NUM_PRODUCTORES) + (NUM_ELEMENTOS_TOTALES % NUM_PRODUCTORES)) : (NUM_ELEMENTOS_TOTALES / NUM_PRODUCTORES));
+            coche(i);
         }
         else
         {
             printf("Generado el productor %d\n", i);
-        }
-    }
-    for ((i = 0); i < NUM_CONSUMIDORES && pid != 0; i++)
-    {
-        pid = fork();
-        if (pid < 0)
-        { // Hubo error, aun asi no abortamos
-            perror("Error en fork()");
-        }
-        else if (pid == 0)
-        {                               // Este es el hijo
-            if (sem_post(num_procesos)) // Incrementamos el semaforo que lleva la cuenta del numero de procesos
-            {
-                perror("Error en sem_post");
-            }
-            consumidor(i==0 ? ((NUM_ELEMENTOS_TOTALES / NUM_CONSUMIDORES) + (NUM_ELEMENTOS_TOTALES % NUM_CONSUMIDORES)) : (NUM_ELEMENTOS_TOTALES / NUM_CONSUMIDORES));
-        }
-        else
-        {
-            printf("Generado el consumidor %d\n", i);
         }
     }
 
@@ -225,25 +220,28 @@ int main(int argc, char **argv)
         perror("Error en sem_getvalue");
     }
 
-    if (munmap(buffer, sizeof(int) * TAM_BUFFER)) // Deshacemos los mapeos de memoria
+    if (munmap(cuenta_izquierda, sizeof(int))) // Deshacemos los mapeos de memoria
     {
         perror("Error en munmap()");
-        // No salimos con EXIT_FAILURE, ya que nos compensa seguir intentando desmapear el resto de memoria
     }
-    if (munmap(cuenta, sizeof(int))) // Deshacemos los mapeos de memoria
+    if (munmap(cuenta_derecha, sizeof(int))) // Deshacemos los mapeos de memoria
     {
         perror("Error en munmap()");
     }
 
-    if (sem_close(empty))
+    if (sem_close(mutex_cuenta_derecha))
     {
         perror("Error en sem_close");
     }
-    if (sem_close(full))
+    if (sem_close(mutex_cuenta_izquierda))
     {
         perror("Error en sem_close");
     }
-    if (sem_close(mutex))
+    if (sem_close(puente))
+    {
+        perror("Error en sem_close");
+    }
+    if (sem_close(cruzando))
     {
         perror("Error en sem_close");
     }
@@ -255,15 +253,19 @@ int main(int argc, char **argv)
     // Cerramos los semaforos. No se cerraran realmente hasta que todos los procesos hayan dejado de usarlos.
     if (valor_semaforo_num_procesos == 0 && pid == 0) // Si este proceso es el ultimo (y estamos seguros de que lo es, porque si vale 0 hemos adquirido el mutex sobre el semaforo), eliminamos los semaforos y el objeto compartido de memoria. Introduzco la condicion pid==0 porque debe cerrarlos uno de los procesos hijo, no el padre.
     {
-        if (sem_unlink("/empty"))
+        if (sem_unlink("/mutex_cuenta_derecha"))
         {
             perror("Error en sem_unlink()");
         }
-        if (sem_unlink("/full"))
+        if (sem_unlink("/mutex_cuenta_izquierda"))
         {
             perror("Error en sem_unlink()");
         }
-        if (sem_unlink("/mutex"))
+        if (sem_unlink("/puente"))
+        {
+            perror("Error en sem_unlink()");
+        }
+        if (sem_unlink("/cruzando"))
         {
             perror("Error en sem_unlink()");
         }
